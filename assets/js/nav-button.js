d23ff5afe6c92b1ce7e622f5966b5d41b47e3d4f
variowhite <variowhite@outlook.com>
2020-06-01 11:06:50 +0700

    add empty space near nav-menu for closing

--- assets/js/nav-button.js
+++ assets/js/nav-button.js
@@ -2,9 +2,11 @@
 var header_nav__button = document.getElementById("header_nav-button");
 var nav = document.getElementById("nav");
 var nav__button_lines = document.getElementById("nav-button_lines");
+var nav_empty__space = document.getElementById("nav_empty-space");
 
 //EVENTS INIT
 header_nav__button.onclick = ButtonClick;
+nav_empty__space.onclick = ButtonClick;
 
 //FUNCTIONS
 function ButtonClick(){
